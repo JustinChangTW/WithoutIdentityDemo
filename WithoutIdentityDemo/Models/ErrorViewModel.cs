71bd7f94104bf9c3343a7b9285ea093fc3f285b0
justin <justin.chang.tw@outlook.com>
2021-03-19 15:14:46 +0800

    加入專案檔案。

---
+++ WithoutIdentityDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WithoutIdentityDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
