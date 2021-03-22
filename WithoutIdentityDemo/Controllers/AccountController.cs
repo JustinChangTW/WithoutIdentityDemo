e2add7f74856e96b6d9d040043de5bd3a4886a3f
justin <justin.chang.tw@outlook.com>
2021-03-22 17:54:30 +0800

    重構session存取, 使用擴充方式

--- WithoutIdentityDemo/Controllers/AccountController.cs
+++ WithoutIdentityDemo/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WithoutIdentityDemo.Models;
+using WithoutIdentityDemo.Extensions;
 
 namespace WithoutIdentityDemo.Controllers
 {
@@ -19,21 +20,23 @@ namespace WithoutIdentityDemo.Controllers
             if(login.Badge=="Justin" && login.CipherCode == "1234567890")
             {
                 //當登入成功後，將使用者資訊存入Session中
-                HttpContext.Session.SetString("Badge", login.Badge);
-                HttpContext.Session.SetString("Role", "administraotr");
+                HttpContext.Session.Set(new UserInfo()
+                {
+                    Name = login.Badge,
+                    Role = "administraotr"
+                });
                 return RedirectToAction(nameof(HomeController.Index), "Home");
             }
             return View(nameof(AccountController.Index), login);
         }
         public IActionResult Logout()
         {
-            var badge = HttpContext.Session.GetString("Badge");
-            var role = HttpContext.Session.GetString("Role");
+            var userinfo = HttpContext.Session.Get<UserInfo>();
 
             HttpContext.Session.Clear();
 
-            badge = HttpContext.Session.GetString("Badge");
-            role = HttpContext.Session.GetString("Role");
+            userinfo = HttpContext.Session.Get<UserInfo>();
+
             return RedirectToAction(nameof(HomeController.Index), "Home");
         }
 
---
+++ WithoutIdentityDemo/Extensions/SessionExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace WithoutIdentityDemo.Extensions
+{
+    public static class SessionExtension
+    {
+        const string KEY= "SESSION";
+        public static void Set<T>(this ISession session,T Data)
+        {
+            session.SetString(KEY, JsonSerializer.Serialize(Data));
+        }
+
+        public static T Get<T>(this ISession session)where T:new ()
+        {
+            var data = session.GetString(KEY);
+            return data!=null?JsonSerializer.Deserialize<T>(data):new T();
+        }
+    }
+}
