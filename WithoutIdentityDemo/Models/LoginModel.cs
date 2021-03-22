b5413b0c50924ad9f603ded704b3dba5292cfe76
justin <justin.chang.tw@outlook.com>
2021-03-22 12:02:28 +0800

    透過ViewModel產生畫面

--- WithoutIdentityDemo/Models/LoginModel.cs
+++ WithoutIdentityDemo/Models/LoginModel.cs
@@ -12,14 +12,14 @@ namespace WithoutIdentityDemo.Models
         /// 識別ID(不使用User、Name、ID等，減少被弱掃或攻擊到的機會)
         /// </summary>
         [Required]
-        [StringLength(30,ErrorMessage ="長度不符合規定，必需介於{0}-{1}之間",MinimumLength =3)]
+        [StringLength(30,ErrorMessage = "長度不符合規定，必需介於{2} - {1}之間", MinimumLength =3)]
         [Display(Name ="帳號")]
         public string Badge { get; set; }
         /// <summary>
         /// 密碼(不使用Password，減少被弱掃或攻擊到的機會)
         /// </summary>
         [Required]
-        [StringLength(30, ErrorMessage = "長度不符合規定，必需介於{0}-{1}之間", MinimumLength = 10)]
+        [StringLength(30, ErrorMessage = "長度不符合規定，必需介於{2} - {1}之間", MinimumLength = 10)]
         [DataType(DataType.Password)]
         [Display(Name ="密碼")]
         public string CipherCode { get; set; }
