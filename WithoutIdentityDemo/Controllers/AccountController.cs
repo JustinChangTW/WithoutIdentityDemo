24c570f0a2dd3fb5b378e3b07d1f580e6af3c564
justin <justin.chang.tw@outlook.com>
2021-04-07 12:14:24 +0800

    login/logout  undone......

---
+++ WithoutIdentityDemo/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WithoutIdentityDemo.Models;
+
+namespace WithoutIdentityDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(LoginModel data)
+        {
+            if (data.Badge == "Justin" && data.CipherCode == "1111111111")
+            {
+                //to do append cookie
+
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+            return View(nameof(HomeController.Index), data);
+        }
+
+        public IActionResult Logout()
+        {
+            //to do clean cookie
+
+            return View();
+        }
+
+    }
+}
---
+++ WithoutIdentityDemo/Models/LoginModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WithoutIdentityDemo.Models
+{
+    public class LoginModel
+    {
+        /// <summary>
+        /// 識別ID(不使用User、Name、ID等，減少被弱掃或攻擊到的機會)
+        /// </summary>
+        [Required]
+        [StringLength(30, ErrorMessage = "長度不符合規定", MinimumLength = 3)]
+        [Display(Name = "帳號")]
+        public string Badge { get; set; }
+        /// <summary>
+        /// 密碼(不使用Password，減少被弱掃或攻擊到的機會)
+        /// </summary>
+        [Required]
+        [StringLength(30, ErrorMessage = "長度不符合規定", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        [Display(Name = "密碼")]
+        public string CipherCode { get; set; }
+    }
+}
